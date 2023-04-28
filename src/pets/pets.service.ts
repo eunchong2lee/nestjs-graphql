5e2547bd0e5e46d0446dcafbeb54263a86f1a3d1
eunchong2lee <tlfqjrjs1234@naver.com>
2023-04-28 12:44:30 +0900

    [Complete]

---
+++ src/pets/pets.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Owner } from 'src/owners/entities/owner.entity';
+import { OwnersService } from 'src/owners/owners.service';
+import { Repository } from 'typeorm';
+import { CreatePetInput } from './dto/create-pet.input';
+import { Pet } from './pet.entity';
+
+@Injectable()
+export class PetsService {
+    constructor(
+        @InjectRepository(Pet) private petsRepository: Repository<Pet>,
+        private ownersService: OwnersService,
+    ) {}
+
+    createPet(createPetInput: CreatePetInput): Promise<Pet> {
+        const newPet = this.petsRepository.create(createPetInput);
+        // newPet = new Pet(); new.name = input.name
+        return this.petsRepository.save(newPet);
+    }
+
+    async findAll(): Promise<Pet[]> {
+        return this.petsRepository.find(); // SELECT * pet
+    }
+
+    findOne(id: number): Promise<Pet> {
+        return this.petsRepository.findOneOrFail({ where: { id } });
+    }
+
+    getOwner(ownerId: number): Promise<Owner> {
+        return this.ownersService.findOne(ownerId);
+    }
+}
